b9d34fca8e865df8ca558da73ac948296dfbc6d3
Dag Wästberg <dwastberg@gmail.com>
2026-03-11 21:15:25 +0100

    add readme warning

--- polyforge/__init__.py
+++ polyforge/__init__.py
@@ -4,7 +4,7 @@ This library provides utilities for processing, simplifying, and manipulating
 polygon geometries using Shapely.
 """
 
-__version__ = "0.1.0a7"
+__version__ = "0.1.0a8"
 
 # Simplification functions
 from .simplify import (
